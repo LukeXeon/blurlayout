3151ba8a84d8ff8c326a9ae3106e4b536099ec3d
发光的半导体 <694014820@qq.com>
2021-08-19 00:03:40 +0800

    m

--- uikit/src/main/java/open/source/uikit/ipc/AnyParcelable.kt
+++ uikit/src/main/java/open/source/uikit/ipc/AnyParcelable.kt
@@ -8,7 +8,10 @@ import java.io.ObjectInputStream
 import java.io.ObjectOutputStream
 
 class AnyParcelable(val value: Any?) : Parcelable {
-    constructor(parcel: Parcel) : this(parcel.readValue(AnyParcelable::class.java.classLoader))
+    constructor(
+        parcel: Parcel,
+        loader: ClassLoader?
+    ) : this(parcel.readValue(loader))
 
     override fun writeToParcel(parcel: Parcel, flags: Int) {
         parcel.writeValue(value)
@@ -18,13 +21,21 @@ class AnyParcelable(val value: Any?) : Parcelable {
         return 0
     }
 
-    companion object CREATOR : Parcelable.Creator<AnyParcelable> {
+    companion object CREATOR : Parcelable.ClassLoaderCreator<AnyParcelable> {
+
         override fun createFromParcel(parcel: Parcel): AnyParcelable {
-            return AnyParcelable(parcel)
+            return createFromParcel(parcel, null)
         }
 
         override fun newArray(size: Int): Array<AnyParcelable?> {
             return arrayOfNulls(size)
+        }
+
+        override fun createFromParcel(
+            source: Parcel,
+            loader: ClassLoader?
+        ): AnyParcelable {
+            return AnyParcelable(source, loader)
         }
     }
 }
